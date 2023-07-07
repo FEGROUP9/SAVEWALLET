c2b17fbcc8e0bc76ba2dca659b6b54e58728f9c1
hookor <gerrard1219@gmail.com>
2023-07-07 16:57:08 +0900

    Feature: Initial setup switching to Styled Components

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from 'pages/Router'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <RouterProvider router={router} />
