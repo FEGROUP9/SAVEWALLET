eb49bd6891cdc222c45502a4930e6048398c7d62
hookor <gerrard1219@gmail.com>
2023-07-07 18:52:05 +0900

    style: device size setting

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import { theme } from 'style/index'
 
 export const App = () => {
   return (
     <>
-      <Outlet />
+      <ThemeProvider theme={theme}>
+        <Outlet />
+      </ThemeProvider>
     </>
   )
 }
