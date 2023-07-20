07d9b814263029d220322d645d68296d8cb633f9
hookor <gerrard1219@gmail.com>
2023-07-20 14:02:28 +0900

    feat: Header.tsx

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,6 +1,44 @@
+import styled from 'styled-components'
+import { ChevronLeftIcon } from '@heroicons/react/outline'
+import { useNavigate } from 'react-router-dom'
+const Wrapper = styled.div`
+  display: flex;
+  .backwards {
+    width: 24px;
+    height: 24px;
+    margin: auto 0;
+    padding: 0 20px;
+    cursor: pointer;
+  }
+  .pathname {
+    vertical-align: baseline;
+    font-size: 40px;
+    letter-spacing: -2px;
+  }
+  @media ${props => props.theme.mobile} {
+  }
+  @media ${props => props.theme.tablet} {
+  }
+  @media ${props => props.theme.laptop} {
+  }
+  @media ${props => props.theme.desktop} {
+  }
+`
 
 export const Header = () => {
+  let navigate = useNavigate()
   return (
-    <div>Header</div>
+    <Wrapper>
+      <a
+        className="backwards"
+        onClick={() => {
+          navigate(-1)
+        }}>
+        <ChevronLeftIcon />
+      </a>
+      <div className="pathname">
+        {window.location.pathname.replace('/', '').toUpperCase()}
+      </div>
+    </Wrapper>
   )
 }
