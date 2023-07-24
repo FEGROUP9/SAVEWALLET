424d7af451638e26a621bc32b91dab41f64c7dea
AHNJOONGHOO <115582699+hookor@users.noreply.github.com>
2023-07-24 17:43:25 +0900

    conf: temporarily exclude removeConsole

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import removeConsole from 'vite-plugin-remove-console'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), removeConsole(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths()],
   server: {
     port: 3000
   }
